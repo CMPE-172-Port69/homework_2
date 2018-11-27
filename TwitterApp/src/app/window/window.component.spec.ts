23b10fc447d7f631af63cd78ebe7a16a96150e79
Robert Campbell <robertgcampbell95@gmail.com>
2018-11-26 20:49:48 -0800

    Remove unnecessary tests

--- TwitterApp/src/app/window/window.component.spec.ts
+++ TwitterApp/src/app/window/window.component.spec.ts
@@ -18,8 +18,4 @@ describe('WindowComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
