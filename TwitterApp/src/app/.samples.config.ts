67b6b5a7cf259bc22015c9aee98e303f4a60ee63
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-03 16:49:58 -0800

    Add okta authentication

---
+++ TwitterApp/src/app/.samples.config.ts
@@ -0,0 +1,11 @@
+export default {
+    oidc: {
+      clientId: '0oahzi162f5nVgbD90h7',
+      issuer: 'https://dev-838726.oktapreview.com/',
+      redirectUri: 'http://localhost:1234/implicit/callback',
+      scope: 'openid profile email',
+    },
+    resoureServer: {
+        messagesUrl: 'http://localhost:8000/api/messages',
+    },
+};
