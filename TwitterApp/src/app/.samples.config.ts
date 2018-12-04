9974cb164bc2d3a1b97c48700009c5eceed3baf1
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-04 00:22:29 -0800

    Update okta information

--- TwitterApp/src/app/.samples.config.ts
+++ TwitterApp/src/app/.samples.config.ts
@@ -1,8 +1,12 @@
 export default {
-  oidc: {
-    clientId: '0oahzi162f5nVgbD90h7',
-    issuer: 'https://dev-838726.oktapreview.com',
-    redirectUri: 'http://localhost:1234/implicit/callback',
-    scope: 'openid profile email',
-  }
+
+    oidc: {
+      clientId: '0oai1f5s7nxjbR5IG0h7',
+      issuer: 'https://dev-838726.oktapreview.com/',
+      redirectUri: 'http://104.248.178.198:1234/implicit/callback',
+      scope: 'openid profile email',
+    },
+    resoureServer: {
+        messagesUrl: 'http://localhost:8000/api/messages',
+    },
 };
