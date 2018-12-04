d14ed651f92773d1df9b13746ed971161977e46e
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-03 17:27:51 -0800

    Update okta authentication

--- TwitterApp/src/app/.samples.config.ts
+++ TwitterApp/src/app/.samples.config.ts
@@ -1,8 +1,8 @@
 export default {
     oidc: {
-      clientId: '0oahzi162f5nVgbD90h7',
+      clientId: '0oahzgi1txGO0DaT80h7',
       issuer: 'https://dev-838726.oktapreview.com/',
-      redirectUri: 'http://localhost:1234/implicit/callback',
+      redirectUri: 'http://104.248.178.198:1234/implicit/callback',
       scope: 'openid profile email',
     },
     resoureServer: {
