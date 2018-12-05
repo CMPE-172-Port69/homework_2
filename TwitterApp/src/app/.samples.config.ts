7af3750c3c136a20338be79a65b99faa953c9d97
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-05 01:17:55 -0800

    Update links for production

--- TwitterApp/src/app/.samples.config.ts
+++ TwitterApp/src/app/.samples.config.ts
@@ -1,8 +1,8 @@
 export default {
   oidc: {
-    clientId: '0oai1gmnxfqLkm3f90h7',
+    clientId: '0oai1f5s7nxjbR5IG0h7',
     issuer: 'https://dev-323697.oktapreview.com',
-    redirectUri: 'http://localhost:1234/implicit/callback',
+    redirectUri: 'http://104.248.178.198:1234/implicit/callback',
     scope: 'openid profile email',
   }
 };
